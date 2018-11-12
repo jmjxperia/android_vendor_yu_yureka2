ffbf94ac040c2c035a8d9fb4fbb6e9c2a8f51d63
jmjxperia <41988303+jmjxperia@users.noreply.github.com>
2018-11-12 13:08:37 +0530

    rebrand

--- msm8953-common/Android.mk
+++ msm8953-common/Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter taco,$(TARGET_DEVICE)),)
+ifneq ($(filter YUREKA2,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := vendor.display.color@1.0
